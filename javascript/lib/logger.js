7c903812b5b39079f47f01429221521c8532c83a
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-11-21 16:11:40 +0530

    feat(logs): add string log prefixes

--- javascript/lib/logger.js
+++ javascript/lib/logger.js
@@ -6,6 +6,17 @@ let FgRed = "\x1b[31m",
     FgMagenta = "\x1b[35m",
     FgWhite = "\x1b[37m";
 
+const printPrefix = "[PRINT]: ",
+    errorPrefix = "[ERROR]: ",
+    warningPrefix = "[WARN]: ",
+    infoPrefix = "[INFO]: ",
+    okPrefix = "[OK]: ",
+    wirePrefix = "[WIRE]: ",
+    debugPrefix = "[DEBUG]: ",
+    hbPrefix = "[HB]: "
+;
+
+
 const active = {
     print: true,
     error: true,
@@ -19,55 +30,63 @@ const active = {
 
 const methods = {
     print: (...str) => {
-        if(active.print) {
+        if (active.print) {
+            str.unshift(printPrefix);
             console.log.apply(console, Array.prototype.slice.call(str));
         }
     },
     error: (...str) => {
-        if(active.error) {
+        if (active.error) {
             str.unshift(FgRed);
+            str.unshift(errorPrefix);
             str.push(Reset);
             console.log.apply(console, Array.prototype.slice.call(str));
         }
     },
     warning: (...str) => {
-        if(active.warning) {
+        if (active.warning) {
             str.unshift(FgYellow);
+            str.unshift(warningPrefix);
             str.push(Reset);
             console.log.apply(console, Array.prototype.slice.call(str));
         }
     },
     info: (...str) => {
-        if(active.info) {
+        if (active.info) {
             str.unshift(FgCyan);
+            str.unshift(infoPrefix);
             str.push(Reset);
             console.log.apply(console, Array.prototype.slice.call(str));
         }
     },
     ok: (...str) => {
-        if(active.ok) {
+        if (active.ok) {
             str.unshift(FgGreen);
+            str.unshift(okPrefix);
             str.push(Reset);
             console.log.apply(console, Array.prototype.slice.call(str));
         }
     },
-    wire: (...str)=>{
-        if(active.wire){
+    wire: (...str) => {
+        if (active.wire) {
             str.unshift(FgMagenta);
+            str.unshift(wirePrefix)
             str.push(Reset);
             console.log.apply(console, Array.prototype.slice.call(str));
         }
     },
-    debug: (...str)=>{
-        if(active.debug){
+    debug: (...str) => {
+        if (active.debug) {
             str.unshift(FgWhite);
+            str.unshift(debugPrefix)
             str.push(Reset);
             console.log.apply(console, Array.prototype.slice.call(str));
         }
     },
     hb: (...str) => {
-        if(active.hb) {
+        if (active.hb) {
             str.unshift(FgYellow);
+            str.unshift(hbPrefix);
             str.push(Reset);
             console.log.apply(console, Array.prototype.slice.call(str));
         }
